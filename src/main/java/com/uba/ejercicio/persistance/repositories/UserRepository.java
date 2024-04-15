19299add132be6870d9101fe4a4006e867933ecc
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-15 19:39:04 -0300

    Add findByEmail method to UserRepository

--- src/main/java/com/uba/ejercicio/persistance/repositories/UserRepository.java
+++ src/main/java/com/uba/ejercicio/persistance/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.uba.ejercicio.persistance.repositories;
 import com.uba.ejercicio.persistance.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
