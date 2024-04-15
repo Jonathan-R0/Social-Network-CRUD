e4042ee741063851ea5f1408e272990bea78fc22
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-14 22:05:32 -0300

    Create entity Profile

---
+++ src/main/java/com/uba/ejercicio/persistance/entities/Profile.java
@@ -0,0 +1,39 @@
+package com.uba.ejercicio.persistance.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private byte[] photo;
+
+    private Date birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "gender_id")
+    private Gender gender;
+
+    @OneToMany
+    private List<Hobbies> hobbies; // TODO delete hobbies when deleting profile
+
+    @OneToOne
+    private User user;
+
+}
