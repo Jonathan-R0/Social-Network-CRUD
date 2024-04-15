743ee12f945a10e8da180171738b6d4d091de547
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-15 19:39:23 -0300

    Add implementation for all UserServiceImpl methods

--- src/main/java/com/uba/ejercicio/services/impl/UserServiceImpl.java
+++ src/main/java/com/uba/ejercicio/services/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.uba.ejercicio.services.impl;
 
 import com.uba.ejercicio.dto.UserDto;
 import com.uba.ejercicio.persistance.entities.User;
+import com.uba.ejercicio.persistance.repositories.UserRepository;
 import com.uba.ejercicio.services.UserService;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,18 +16,26 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public User createUser(UserDto user) {
-        return null;
+        return userRepository.save(
+                User.builder()
+                    .email(user.getEmail())
+                    .password(passwordEncoder.encode(user.getPassword()))
+                    .build()
+        );
     }
 
     @Override
     public User getUserByEmail(String email) {
-        return null;
+        return userRepository.findByEmail(email).orElseThrow();
     }
 
     @Override
     public void deleteUserByEmail(String email) {
-
+        userRepository.delete(getUserByEmail(email));
     }
 }
