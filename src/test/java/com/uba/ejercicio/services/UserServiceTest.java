4ef316be78ec474f66b81c8c82ad9e1c1183fab4
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-15 19:25:12 -0300

    Create UserService test file

---
+++ src/test/java/com/uba/ejercicio/services/UserServiceTest.java
@@ -0,0 +1,22 @@
+package com.uba.ejercicio.services;
+
+import com.uba.ejercicio.persistance.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        userRepository.deleteAll();
+    }
+
+}
