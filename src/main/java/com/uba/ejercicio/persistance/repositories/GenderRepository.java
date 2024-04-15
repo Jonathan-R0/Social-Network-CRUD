cfbf0e7fc17fa66d490ab6daeaadc8744391f654
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-15 01:25:38 -0300

    Create Gender entity repository

--- src/main/java/com/uba/ejercicio/persistance/repositories/GenderRepository.java
+++ src/main/java/com/uba/ejercicio/persistance/repositories/GenderRepository.java
@@ -3,5 +3,10 @@ package com.uba.ejercicio.persistance.repositories;
 import com.uba.ejercicio.persistance.entities.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GenderRepository extends JpaRepository<Gender, Long> {
+
+    Optional<Gender> findByName(String name);
+
 }
