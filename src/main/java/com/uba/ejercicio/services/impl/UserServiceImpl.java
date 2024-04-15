2f5ad7b14fdfcd5d84f77140916bd8c29ebd65f2
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-15 19:30:12 -0300

    Create method signature for user service

--- src/main/java/com/uba/ejercicio/services/impl/UserServiceImpl.java
+++ src/main/java/com/uba/ejercicio/services/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.uba.ejercicio.services.impl;
 
+import com.uba.ejercicio.dto.UserDto;
+import com.uba.ejercicio.persistance.entities.User;
 import com.uba.ejercicio.services.UserService;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,4 +9,18 @@ import org.springframework.stereotype.Service;
 @Service
 @NoArgsConstructor
 public class UserServiceImpl implements UserService {
+    @Override
+    public User createUser(UserDto user) {
+        return null;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public void deleteUserByEmail(String email) {
+
+    }
 }
