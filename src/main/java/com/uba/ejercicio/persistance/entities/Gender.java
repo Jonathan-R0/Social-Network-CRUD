b365d2ded00d40783249f4bbbd61b157b49ac709
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-14 22:04:50 -0300

    Create entity Gender

---
+++ src/main/java/com/uba/ejercicio/persistance/entities/Gender.java
@@ -0,0 +1,21 @@
+package com.uba.ejercicio.persistance.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Gender {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+}
