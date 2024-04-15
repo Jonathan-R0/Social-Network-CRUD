517175e30a237ba70101ff2ee9002ea06a772931
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-15 19:34:33 -0300

    Add password encoder bean

--- src/main/java/com/uba/ejercicio/services/impl/UserServiceImpl.java
+++ src/main/java/com/uba/ejercicio/services/impl/UserServiceImpl.java
@@ -4,11 +4,17 @@ import com.uba.ejercicio.dto.UserDto;
 import com.uba.ejercicio.persistance.entities.User;
 import com.uba.ejercicio.services.UserService;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @NoArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public User createUser(UserDto user) {
         return null;
