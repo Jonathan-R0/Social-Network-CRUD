7b22181d73c34e62884cd793e289c7dc2d6d0e27
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-15 01:26:31 -0300

    Create http security config class

---
+++ src/main/java/com/uba/ejercicio/configuration/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.uba.ejercicio.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().permitAll();
+        http.cors().and().csrf().disable();
+        return http.build();
+    }
+
+}
