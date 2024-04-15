721a263b91d67fcb17194dd6ae99e5c1688cfc2b
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-14 22:05:09 -0300

    Create entity Hobbies

---
+++ src/main/java/com/uba/ejercicio/persistance/entities/Hobbies.java
@@ -0,0 +1,22 @@
+package com.uba.ejercicio.persistance.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Hobbies {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
