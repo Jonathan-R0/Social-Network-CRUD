cb39a04b14973c873842838febff37b1dc4603eb
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-20 03:32:25 -0300

    Update SecurityConfiguration to add whitelisting, request filtering and bean authenticationManager creation

--- src/main/java/com/uba/ejercicio/configuration/SecurityConfiguration.java
+++ src/main/java/com/uba/ejercicio/configuration/SecurityConfiguration.java
@@ -1,33 +1,61 @@
 package com.uba.ejercicio.configuration;
 
+import com.uba.ejercicio.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    private UserService userService;
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-ui/**",
+            "/swagger",
+            "/api-docs/**",
+            "/api/v1/user/password/forgot",
+            "/api/v1/user/*/password/recover/**",
+            "/api/v1/token",
+            "/login",
+    };
+
+    @Autowired
+    private AuthenticationFilter authenticationFilter;
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(
-                        authorizeRequests -> authorizeRequests
-                                .requestMatchers("**").permitAll()
-                )
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .build();
+        http.authorizeHttpRequests(
+                    authorizeRequests -> {
+                        authorizeRequests.requestMatchers(AUTH_WHITELIST).permitAll();
+                        authorizeRequests.anyRequest().authenticated();
+                    }
+            )
+            .cors()
+            .and()
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class).build();
     }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder)
+            throws Exception {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder)
+                .and().build();
     }
 
 }
