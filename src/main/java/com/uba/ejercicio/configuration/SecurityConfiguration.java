48c9bc3b333f9637a62f31d7eb28dc4109e7c6e0
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-21 18:17:27 -0300

    Extract AuthenticationManager bean creation from SecurityConfiguration class to AuthenticationManagerProvider class

--- src/main/java/com/uba/ejercicio/configuration/SecurityConfiguration.java
+++ src/main/java/com/uba/ejercicio/configuration/SecurityConfiguration.java
@@ -1,25 +1,18 @@
 package com.uba.ejercicio.configuration;
 
-import com.uba.ejercicio.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-
-    @Autowired
-    private UserService userService;
 
     private static final String[] AUTH_WHITELIST = {
             "/swagger-ui/**",
@@ -47,15 +40,6 @@ public class SecurityConfiguration {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class).build();
-    }
-
-    @Bean
-    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder)
-            throws Exception {
-        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userService)
-                .passwordEncoder(passwordEncoder)
-                .and().build();
     }
 
 }
