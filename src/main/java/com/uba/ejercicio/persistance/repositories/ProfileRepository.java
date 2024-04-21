d7a4b0278045fd13315d53700eed3b2cddc3281a
Azul Fuentes <azul.fuentes@amaze.co>
2024-04-21 16:31:34 -0300

    add findByName to hobbies and findById to profile repositories

--- src/main/java/com/uba/ejercicio/persistance/repositories/ProfileRepository.java
+++ src/main/java/com/uba/ejercicio/persistance/repositories/ProfileRepository.java
@@ -3,5 +3,9 @@ package com.uba.ejercicio.persistance.repositories;
 import com.uba.ejercicio.persistance.entities.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+
+    Optional<Profile> findProfileById(String userId);//TODO que sea optional
 }
