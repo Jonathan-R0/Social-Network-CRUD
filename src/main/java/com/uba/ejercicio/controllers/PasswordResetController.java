ad8d3e7e196c82807b8f36317bfbe56bbd412822
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-23 19:38:46 -0300

    Add changePassword method to PasswordResetController

--- src/main/java/com/uba/ejercicio/controllers/PasswordResetController.java
+++ src/main/java/com/uba/ejercicio/controllers/PasswordResetController.java
@@ -1,8 +1,11 @@
 package com.uba.ejercicio.controllers;
 
+import com.uba.ejercicio.dto.ChangePasswordDto;
 import com.uba.ejercicio.dto.RecoverPasswordRequestDto;
 import com.uba.ejercicio.dto.ResetPasswordRequestDto;
 import com.uba.ejercicio.services.PasswordResetService;
+import com.uba.ejercicio.services.TokenService;
+import com.uba.ejercicio.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,12 @@ public class PasswordResetController {
 
     @Autowired
     private PasswordResetService passwordResetService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping("/forgot")
     public ResponseEntity<Void> resetPassword(@RequestBody @Valid ResetPasswordRequestDto request) {
@@ -28,6 +37,16 @@ public class PasswordResetController {
             @RequestBody @Valid RecoverPasswordRequestDto request
     ) {
         passwordResetService.recoverPassword(userId, token, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> changePassword(
+            @RequestBody @Valid ChangePasswordDto userDto,
+            @RequestHeader("Authorization") String tokenHeader
+    ) {
+        userService.updatePassword(tokenService.getEmailFromHeader(tokenHeader),
+                userDto.getOldPassword(), userDto.getNewPassword());
         return ResponseEntity.ok().build();
     }
 
