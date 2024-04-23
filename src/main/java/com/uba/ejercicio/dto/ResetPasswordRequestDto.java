a1156056ec720abac82316ce948a8be87d80be46
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-23 16:38:54 -0300

    Add @Email jakarta validation to email attribute in ResetPasswordRequestDto

--- src/main/java/com/uba/ejercicio/dto/ResetPasswordRequestDto.java
+++ src/main/java/com/uba/ejercicio/dto/ResetPasswordRequestDto.java
@@ -1,10 +1,12 @@
 package com.uba.ejercicio.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ResetPasswordRequestDto {
     @NotBlank
+    @Email
     private String email;
 }
