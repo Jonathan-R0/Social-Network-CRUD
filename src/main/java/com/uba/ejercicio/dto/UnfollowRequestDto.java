6a35ff1c54143b7d76c577b4e91611b473a19917
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-24 01:51:23 -0300

    Remove unused import

--- src/main/java/com/uba/ejercicio/dto/UnfollowRequestDto.java
+++ src/main/java/com/uba/ejercicio/dto/UnfollowRequestDto.java
@@ -2,7 +2,6 @@ package com.uba.ejercicio.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
