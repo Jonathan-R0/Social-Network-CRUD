941d94938108a7ae2487ed75ec4fa98c8ba9be52
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-24 01:50:49 -0300

    Configure role access for userFollower/ing endpoints

--- src/main/java/com/uba/ejercicio/configuration/SecurityConfiguration.java
+++ src/main/java/com/uba/ejercicio/configuration/SecurityConfiguration.java
@@ -53,6 +53,8 @@ public class SecurityConfiguration {
                                 .hasAnyAuthority(ADMIN_ROLE, USER_ROLE);
                         authorizeRequests.requestMatchers(HttpMethod.POST, "/api/v1/user/gender")
                                 .hasAuthority(ADMIN_ROLE);
+                        authorizeRequests.requestMatchers("/api/v1/user/*/follower", "/api/v1/user/*/following")
+                                .hasAnyAuthority(ADMIN_ROLE, USER_ROLE);
                         authorizeRequests.anyRequest().authenticated();
                     }
             )
