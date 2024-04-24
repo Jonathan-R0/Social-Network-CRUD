8d3451a52dd183f920f67f1b3924c4661b6d8e31
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-04-24 04:15:43 -0300

    Add controller role restrictions to profile controllers

--- src/main/java/com/uba/ejercicio/configuration/SecurityConfiguration.java
+++ src/main/java/com/uba/ejercicio/configuration/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration {
                                 .hasAnyAuthority(ADMIN_ROLE, USER_ROLE);
                         authorizeRequests.requestMatchers(HttpMethod.POST, "/api/v1/user/gender")
                                 .hasAuthority(ADMIN_ROLE);
-                        authorizeRequests.requestMatchers("/api/v1/user/*/follower", "/api/v1/user/*/following")
+                        authorizeRequests.requestMatchers("/api/v1/user/*/follower", "/api/v1/user/*/following", "/api/v1/user/*/profile")
                                 .hasAnyAuthority(ADMIN_ROLE, USER_ROLE);
                         authorizeRequests.anyRequest().authenticated();
                     }
